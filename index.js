8adb810905ec96d1da6d2475553d67be726c83d0
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-12 12:48:51 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-// run `node index.js` in the terminal
+// this is curd app demo in nodejs
+const fs = require('fs');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// fs.mkdirSync('folder1');
+//creates the directory
+
+// fs.writeFileSync('folder1/test.txt', 'Hi my name is Abhisek.');
+//creates file and add content to it
+
+// fs.appendFileSync('folder1/test.txt', ' I am 22 years old but still single.');
+// add content to the file
+
+// fs.renameSync('folder1/test.txt', 'folder1/abhisek.txt');
+//to rename the file
+
+// const data = fs.readFileSync('folder1/abhisek.txt', 'utf8');
+// console.log(data);
+//output the data in console
+
